1cdfbb84d64861ded269c0ba96d847531156468e
Yuruch <fuceqwertyt@gmail.com>
2024-09-04 15:27:45 +0300

    fix flake8

--- app/parse.py
+++ app/parse.py
@@ -5,7 +5,10 @@ from typing import List
 from urllib.parse import urljoin
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    ElementNotInteractableException
+)
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
@@ -16,8 +19,14 @@ HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
 PARSE_URLS = {
     "home": urljoin(BASE_URL, "test-sites/e-commerce/more"),
     "computers": urljoin(BASE_URL, "test-sites/e-commerce/more/computers"),
-    "laptops": urljoin(BASE_URL, "test-sites/e-commerce/more/computers/laptops"),
-    "tablets": urljoin(BASE_URL, "test-sites/e-commerce/more/computers/tablets"),
+    "laptops": urljoin(
+        BASE_URL,
+        "test-sites/e-commerce/more/computers/laptops"
+    ),
+    "tablets": urljoin(
+        BASE_URL,
+        "test-sites/e-commerce/more/computers/tablets"
+    ),
     "phones": urljoin(BASE_URL, "test-sites/e-commerce/more/phones"),
     "touch": urljoin(BASE_URL, "test-sites/e-commerce/more/phones/touch"),
 }
@@ -37,9 +46,13 @@ PRODUCT_FIELDS = [field.name for field in fields(Product)]
 
 def parse_single_product(product: WebElement) -> Product:
     return Product(
-        title=product.find_element(By.CLASS_NAME, "title").get_property("title"),
+        title=product.find_element(
+            By.CLASS_NAME, "title"
+        ).get_property("title"),
         description=product.find_element(By.CLASS_NAME, "description").text,
-        price=float(product.find_element(By.CLASS_NAME, "price").text.replace("$", "")),
+        price=float(
+            product.find_element(By.CLASS_NAME, "price").text.replace("$", "")
+        ),
         rating=len(product.find_elements(By.CLASS_NAME, "ws-icon-star")),
         num_of_reviews=int(
             product.find_element(By.CLASS_NAME, "review-count").text.split()[0]
@@ -55,7 +68,7 @@ def parse_single_page(url: str, driver: webdriver.Chrome) -> List[Product]:
         cookie_button = driver.find_elements(By.CLASS_NAME, "acceptCookies")
         if cookie_button:
             cookie_button[0].click()
-    except (NoSuchElementException, ElementNotInteractableException) as e:
+    except (NoSuchElementException, ElementNotInteractableException):
         print("Cookie button not found or already accepted")
 
     while True:
